2f72bd404c78d8fd7d4b7e68c6b3f94032081a2e
Ana Arellano <117821773+acarecor@users.noreply.github.com>
2023-06-13 13:21:23 +0200

    updated index.js whit express.static function and error handling

--- index.js
+++ index.js
@@ -1,65 +1,79 @@
 const express = require ('express');
+const morgan = require('morgan');
+
 const app = express();
 
+// JSON object with data about 10  top movies 
 let topMovies = [
+    
     {
         title:'Citizen Kane',
         director:'Orson Welles',
-        year: '1941'
+        year: '1941',
     },
     {
         title:'Bicycle Thieves',
         director:'Vittorio De Sica',
-        year: '1948'
+        year: '1948',
     },
     {
         title:"Schindler's List",
         director:'Steven Spielberg',
-        year: '1993'
+        year: '1993',
     },
     {
         title:'Il Postino',
         director:'Michael Radford',
-        year: '1995'
+        year: '1995',
     },
     {
         title:'The Godfather',
         director:'Francis Ford Coppola',
-        year: '1972'
+        year: '1972',
     },
     {
         title:'Casablanca',
         director:'Michael Curtiz',
-        year: '1942'
+        year: '1942',
     },
     {
         title:'Life is Beautiful',
         director:'Roberto Benigni',
-        year: '1998'
+        year: '1998',
     },
     {
         title:'Dead Poets Society',
         director:'Peter Weir',
-        year: '1989'
+        year: '1989',
     },
     {
         title:'The Matrix',
         director:'The Wachowsky Brothers',
-        year: '1999'
+        year: '1999',
     },
     {
         title:'The Shawshank Redemption',
         director:'Frank Darabont',
-        year: '1994'
+        year: '1994',
     }
 ];
+
+// morgan  function use
+
+app.use(morgan ('common'));
+
 //GET request
 app.get ('/', (req, res) => {
     res.send('Welcome to myFlix');
 });
 
-app.get('/documentation', (req, res) => {
-    res.sendFile('public/documentation.htms', { root:__dirname});
+// express.static function for the public folder containing the documentation file
+
+app.use(express.static('public'));
+
+app.use((err, req, res, next)=> {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
 });
 
 app.get('/movies', (req, res) => {

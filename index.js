e3a2ec1a377512c1631dcb2a4b0cd175e0949a98
Ana Arellano <117821773+acarecor@users.noreply.github.com>
2023-07-04 09:25:00 +0200

    added hashedPassword to users

--- index.js
+++ index.js
@@ -39,6 +39,7 @@ app.get('/', (req, res) => {
 //CREATE a new user account add inn JSON format (mongoose)
 
 app.post('/users', (req, res) => {
+  let hashedPassword = Users.hashPassword(req.body.Password);
   Users.findOne({ Username: req.body.Username })
     .then((user) => {
       if (user) {
@@ -46,7 +47,7 @@ app.post('/users', (req, res) => {
       } else {
         Users.create({
           Username: req.body.Username,
-          Password: req.body.Password,
+          Password: hashedPassword,
           Email: req.body.Email,
           Birthday: req.body.Birthday,
         })

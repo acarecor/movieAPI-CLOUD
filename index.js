bf451fc0a754d11dcc1e8f4c7321a0895cd03d62
Ana Arellano <117821773+acarecor@users.noreply.github.com>
2023-08-17 15:16:20 +0200

    changed CORS configuration

--- index.js
+++ index.js
@@ -9,7 +9,20 @@ const mongoose = require('mongoose');
 const Models = require('./models.js');
 
 const cors= require('cors');
-app.use(cors());
+//added allowed Origins 
+//app.use(cors());
+
+let allowedOrigins = [ 'http://localhost:8080', 'http://localhost:1234', 'https://myflix-cineflix.netlify.app'];
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      let message = "The CORS policy for this application doesn't allow access from origin" + origin;
+      return callback(new Error(message), false);
+    }
+    return callback(null, true);
+  }
+}));
 
 const Movies = Models.Movie;
 const Users = Models.User;
